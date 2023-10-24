a9e5dc6393ea9d742333fe8e4b9b44e5a025db53
Mehmet Akif CIFTCI <17935050+themanoftalent@users.noreply.github.com>
2023-10-24 18:29:35 +0200

    Update and rename work4answer.java to Work4answer.java
    Please see the answer

---
+++ Work4answer.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double num1, num2, result;
+        char operator;
+
+        System.out.print("Enter the first number: ");
+        num1 = input.nextDouble();
+
+        System.out.print("Enter an operator (+, -, *, /): ");
+        operator = input.next().charAt(0);
+
+        System.out.print("Enter the second number: ");
+        num2 = input.nextDouble();
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                System.out.println("Result: " + num1 + " + " + num2 + " = " + result);
+                break;
+            case '-':
+                result = num1 - num2;
+                System.out.println("Result: " + num1 + " - " + num2 + " = " + result);
+                break;
+            case '*':
+                result = num1 * num2;
+                System.out.println("Result: " + num1 + " * " + num2 + " = " + result);
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                    System.out.println("Result: " + num1 + " / " + num2 + " = " + result);
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operator.");
+        }
+    }
+}
+
