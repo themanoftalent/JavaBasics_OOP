80c5035d3d31cfd3b93d43acc96937a074a658e0
Adem Buran <116068005+AdBuran@users.noreply.github.com>
2023-10-27 10:20:28 +0300

    Last two assignments are done

---
+++ Assigned/factorialCalculator.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class factorialCalculator {
+	public static void main(String[] args ) {
+//		Factorial Calculator
+int userNumber=1 , stepNumber=1;
+Scanner userInput = new Scanner(System.in);
+userNumber = userInput.nextInt();
+if (userNumber == 0) {
+System.out.println("1");
+}else if(userNumber <0) {
+System.out.println("please enter a nonnegative number");
+}else {
+System.out.println("Calculation process has been start ..."+"\n"+"Calculation is done.");
+
+for(int followingNumber = 1 ; followingNumber <= userNumber ; followingNumber ++) {
+	
+	stepNumber = stepNumber * followingNumber;
+	
+}
+userNumber = stepNumber;
+System.out.println(userNumber);
+}
+userInput.close();
+ }
+}
---
+++ Assigned/randomNumberGame.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+public class randomNumberGame {
+	public static void main (String [] args ) {
+Scanner gameAnswer = new Scanner(System.in);
+Random gameRanInt = new Random();
+int gameNumber = gameRanInt.nextInt(100);
+boolean isAnswerCorrect = true;
+System.out.print("Enter your answer");
+do {
+int userNumber = gameAnswer.nextInt();
+if (userNumber == gameNumber) {
+System.out.println("Correct . \n you got the lucky number");
+isAnswerCorrect = false;
+}else if (userNumber < gameNumber){
+System.out.println("Not correct the lucky number is bigger than yours answer try again");
+}else {
+System.out.println("Not correct the lucky number is less than your's answer try again");}
+}while(isAnswerCorrect);
+gameAnswer.close();
+}
+}
