79858cecdce0de604bf52c19ad67bffb43017272
EmiralpYakali <162481842+EmiralpYakali@users.noreply.github.com>
2024-03-11 16:16:26 +0300

    Emiralp Yakalı namesurname week3

--- Homework_week3/Ders.java
+++ Homework_week3/Ders.java
@@ -2,23 +2,15 @@ import java.util.Scanner;
 public class Ders {
 	public static void main(String[] args) {
 		
-double toplam,cikarma,bolme,carpma;
-  Scanner scanner = new Scanner(System.in);
+  Scanner emirscanner = new Scanner(System.in);
 
-System.out.print("Birinci sayıyı girin: ");
-int sayi1 = scanner.nextInt();
+System.out.print("Adınızı Girin: ");
+String ad = emirscanner.nextLine();
 
-System.out.print("İkinci sayıyı girin: ");
-int sayi2 = scanner.nextInt();
+System.out.print("Soyadınızı girin: ");
+String soyad = emirscanner.nextLine();
 
-toplam=sayi1+sayi2;
-cikarma=sayi1-sayi2;
-bolme=sayi1/sayi2;
-carpma=sayi1*sayi2;
-/*System.out.println(toplam);
-System.out.println(cikarma);
-System.out.println(carpma);
-System.out.println(bolme);*/
-System.out.println(toplam+" "+cikarma+" "+carpma+" "+bolme);
+
+System.out.println(ad+" "+soyad);
 }
 }
