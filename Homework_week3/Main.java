7af08660456ce87a9742c83ad57b498b75c7a5d1
Voyager256 <111907621+Voyager256@users.noreply.github.com>
2024-03-11 15:39:44 +0300

    Bera Çakıcı hafta 3 ödev

---
+++ Homework_week3/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+        int sayi1 = 5;
+        int sayi2 = 3;
+        int toplam = sayi1 + sayi2;
+        System.out.println(toplam);
+
+
+        int fark = sayi1 - sayi2;
+        System.out.println(fark);
+
+        int carpim = sayi1 * sayi2;
+        System.out.println(carpim);
+
+        int bolum = sayi1 / sayi2;
+    }
+}
