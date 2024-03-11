775c1070334495206518f2e8df543297013dc4fb
BKaanSahin <162595080+BKaanSahin@users.noreply.github.com>
2024-03-11 15:47:53 +0300

    Kaan Şahin 2311502223

---
+++ Homework_week3/Main.java
@@ -0,0 +1,13 @@
+import  java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int a,b;
+        a=10;
+        b=5;
+        System.out.println("toplam:" +(a+b));
+        System.out.println("fark:" +(a-b));
+        System.out.println("carpım:" +(a*b));
+        System.out.println("bolum:" +(a/b));
+
+    }
+}
