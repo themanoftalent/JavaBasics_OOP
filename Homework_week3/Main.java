72bbec6d2348d58ba4cd10df6f110223697a91dc
Arda Göktaş <ardagoktas2004@gmail.com>
2024-03-11 16:25:27 +0300

    Arda Göktaş week3 Homework2

--- Homework_week3/Main.java
+++ Homework_week3/Main.java
@@ -2,31 +2,13 @@ import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static Scanner input = new Scanner(System.in);
-
     public static void main(String[] args) {
-        int a,b,sum,extraction,division,multiplication,remainder;
-        System.out.println("Enter the first number");
-        a = input.nextInt();
-        System.out.println("Enter the second number");
-        b= input.nextInt();
-        sum = a+b;
-        System.out.println("Sum of a and b " +sum);
+        System.out.println("Please Enter your name: ");
+        String name = input.next();
+        System.out.println("Please enter your surname");
+        String surname = input.next();
 
-        extraction = a-b;
-        System.out.println("Extraction of a and b " +extraction);
-
-        division = a/b;
-        System.out.println("Division of a and b " +division);
-
-        multiplication = a*b;
-        System.out.println("Multiply of a and b " +multiplication);
-
-        remainder = a%b;
-        System.out.println("Remainder of a and b " +remainder);
-
-
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.println("Your name is:" + name +" "+ surname);
 
     }
 }
