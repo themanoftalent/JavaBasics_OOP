7791da530bb48aec51f892c3397b1db1a23505e2
Yusuf Mermertaş <154246812+yusufmrmrts18@users.noreply.github.com>
2024-03-11 16:19:02 +0300

    Yusuf Mermertas second homework

--- Homework_week3/Main.java
+++ Homework_week3/Main.java
@@ -3,15 +3,11 @@ import java.util.Scanner;
 public class Main {
     static Scanner input =new Scanner(System.in);
     public static void main(String[] args) {
-        double a,b,toplam,fark,bolme,carpma;
-        a=input.nextInt();
-        b=input.nextInt();
-        carpma=a*b;
-        bolme=a/b;
-        fark=a-b;
-        toplam=a+b;
-        System.out.println("toplam="+toplam);
-        System.out.println("fark="+fark);
-        System.out.println("carpma="+carpma);
-        System.out.println("bolme="+bolme);
+        System.out.println("isminiz ne");
+        String isim;
+        isim =input.next();
+        System.out.println("soy isminizi giriniz");
+        String soyisim;
+        soyisim=input.next();
+        System.out.println(isim +" " +soyisim);
     }}
