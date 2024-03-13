33520b618dfc22b7945040d8f219488358ecb5c1
kostkastanis <163309981+kostkastanis@users.noreply.github.com>
2024-03-13 17:58:00 +0300

    stanislas kostka sidibe print my name

--- Homework_week3/Main.java
+++ Homework_week3/Main.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-
 public class Main {
-    static Scanner input =new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("isminiz ne");
-        String isim;
-        isim =input.next();
-        System.out.println("soy isminizi giriniz");
-        String soyisim;
-        soyisim=input.next();
-        if((isim.equals("yusuf"))&&(soyisim.equals("mermertas")))
-        {
-            System.out.println("isim ve soyisim dogru");
-        }
-        else
-        {
-            System.out.println("isim ve soyisim dogru degil");
-        }
-        System.out.println(isim +" " +soyisim);
-    }}
+        System.out.println("Please enter your name");
+        String name;
+        name = input.next();
+        System.out.println("Please enter your surname");
+        String surname;
+        surname = input.next();
+        System.out.println("Your name is " +name +". Your surname is "+surname);
+
+    }
+
+}
