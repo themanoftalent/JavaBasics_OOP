78afe60ad3e2069bdecae792d229661c4432d39d
kostkastanis <163309981+kostkastanis@users.noreply.github.com>
2024-03-13 17:51:22 +0300

    stanislas kostka sidibe math operations

---
+++ Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 3;
+        int sum = num1 + num2;
+        int extraction = num1 - num2;
+        int division = num1 / num2;
+        int multiply = num1 * num2;
+        int remainder = num1 % num2;
+        System.out.println("Sum of num1 and num2 is: " + sum);
+        System.out.println("Extraction of num1 and num2 is: " + extraction);
+        System.out.println("Division: " + division);
+        System.out.println("Multiply: " + multiply);
+        System.out.print("Remainder: " + remainder);
+
+
+    }
+}
