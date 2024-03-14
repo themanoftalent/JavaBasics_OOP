795aee722a3f9e4729e047a6b25cd20b7246ea78
lezizouu <153992448+lezizouu@users.noreply.github.com>
2024-03-14 17:23:51 +0300

    segbe abdoul aziz diomande week3 work1

---
+++ Main.java
@@ -0,0 +1,27 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        int num1=10;
+        int num2=2;
+        int sum;
+        int substrac;
+        int divis;
+        int mutipli;
+        int remaind;
+
+        sum = num1 + num2;
+        substrac = num1 - num2;
+        divis = num1 / num2;
+        mutipli = num1 * num2;
+        remaind = num1 % num2;
+
+        System.out.println("\nsum:" +sum);
+        System.out.println("substraction :" +substrac);
+        System.out.println("division :"+divis);
+        System.out.println("multiplication:" +mutipli);
+        System.out.println("remainder :" +remaind);
+
+
+    }
+}
