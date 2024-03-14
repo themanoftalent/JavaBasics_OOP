bcc9a701351f8184c25fe458496a8e974472c9b6
lezizouu <153992448+lezizouu@users.noreply.github.com>
2024-03-14 17:39:54 +0300

    segbe abdoul aziz diomande week3 work2

--- Homework_week3/Main.java
+++ Homework_week3/Main.java
@@ -1,21 +1,15 @@
 import java.util.Scanner;
-
 public class Main {
-    static Scanner input =new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("isminiz ne");
-        String isim;
-        isim =input.next();
-        System.out.println("soy isminizi giriniz");
-        String soyisim;
-        soyisim=input.next();
-        if((isim.equals("yusuf"))&&(soyisim.equals("mermertas")))
-        {
-            System.out.println("isim ve soyisim dogru");
-        }
-        else
-        {
-            System.out.println("isim ve soyisim dogru degil");
-        }
-        System.out.println(isim +" " +soyisim);
-    }}
+        Scanner scanner=new Scanner(System.in);
+        String name;
+        System.out.println("enter your name");
+        name=scanner.next();
+        String surname;
+        System.out.println("enter your surname");
+        surname=scanner.next();
+        System.out.println("the name is "+ name + " the surname is "+ surname);
+
+    }
+}
