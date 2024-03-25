da484d5ea1ce5ad55239ebf67e3c72c9620d78c3
Arda Göktaş <ardagoktas2004@gmail.com>
2024-03-25 15:45:19 +0300

    Arda Göktaş 2311502241 Switch Case Example 2

--- Homework_week4/Main.java
+++ Homework_week4/Main.java
@@ -1,32 +1,33 @@
+// Arda Göktaş 2311502241
 import java.util.Scanner;
 public class Main {
     static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        double not1,not2,not3,not4,not5, gpa;
-        System.out.println("Enter your first note: ");
-        not1 = input.nextDouble();
-        System.out.println("Enter your second note: ");
-        not2 = input.nextDouble();
-        System.out.println("Enter your third note: ");
-        not3 = input.nextDouble();
-        System.out.println("Enter your fourth note: ");
-        not4 = input.nextDouble();
-        System.out.println("Enter your fifth note: ");
-        not5 = input.nextDouble();
-        gpa = (not1 + not2 + not3 + not4 + not5)/5;
-        int pass = 2;
-        if (gpa >50){
-            pass = 1;
-        }
-
-        switch (pass){
+        int age;
+        System.out.println("Please Enter your age: ");
+        age = input.nextInt();
+        switch (age){
+            case 0:
+                System.out.println("You are just borned:)");
+                break;
             case 1:
-                System.out.println("You passed:)");
+                System.out.println("You learnt how to walk");
                 break;
             case 2:
-                System.out.println("You Failed!!");
+                System.out.println("You learnt how to speak");
                 break;
-
+            case 3:
+                System.out.println("You started to go kindergarten");
+                break;
+            case 4:
+                System.out.println("You are growing up fast");
+                break;
+            case 5:
+                System.out.println("You will start elementary school next year:)");
+                break;
+            default:
+                System.out.println("You are getting old:)");
+                break;
         }
     }
 }
