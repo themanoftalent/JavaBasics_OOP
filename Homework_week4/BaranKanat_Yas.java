e8d90687ff678ea02a3026af560d156468d231d7
Laedon <115706348+Laedon@users.noreply.github.com>
2024-03-25 15:52:45 +0300

    Update and rename Yas.java to BaranKanat_Yas.java

--- Homework_week4/BaranKanat_Yas.java
+++ Homework_week4/BaranKanat_Yas.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Yas {
+public class BaranKanat_Yas {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Yaşınızı Giriniz");
