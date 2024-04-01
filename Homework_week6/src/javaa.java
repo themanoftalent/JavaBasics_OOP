f7f7215e83e4fe3a3c13927960180caec304b1f0
Yusuf Mermertaş <154246812+yusufmrmrts18@users.noreply.github.com>
2024-04-01 15:51:47 +0300

    Yusuf Mermertaş 6. hafta dev

---
+++ Homework_week6/src/javaa.java
@@ -0,0 +1,15 @@
+public class javaa {
+    public static void main(String[] args) {
+        MyClass ornek=new MyClass();
+        System.out.println("Name = "+ornek.name);
+        System.out.println("Surname = "+ornek.surName);
+        System.out.println("Age = "+ornek.age);
+        System.out.println("University = "+ornek.universtiy);
+        System.out.println("Department = "+ornek.department);
+        System.out.println("Hair color = " +ornek.hairColor );
+        System.out.println("Height = "+ornek.heigth);
+        System.out.println("Mustache = "+ornek.mustache);
+        System.out.println("Beard = "+ornek.beard);
+        System.out.println("My hooby = "+ornek.myHobby);
+    }
+}
