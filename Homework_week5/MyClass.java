1faaddc835095be48e2cc0abbc616b782e489786
Arda Göktaş <ardagoktas2004@gmail.com>
2024-04-01 15:35:25 +0300

    Arda Göktaş 2311502241 Week5 MyClass homework

---
+++ Homework_week5/MyClass.java
@@ -0,0 +1,13 @@
+// Arda Göktaş 2311502241
+public class MyClass {
+    public String name = "Arda";
+    public String surname = "Goktas";
+    public int age = 20;
+    public String university = "Bandirma";
+    public String department = "Computer Engineering";
+    public String hairColor = "Brown";
+    public int height = 184;
+    public boolean mustache = true;
+    public boolean beard = true;
+    public String hobby= "Basketball";
+}
---
+++ Homework_week5/MyClassDrive.java
@@ -0,0 +1,15 @@
+// Arda Göktaş 2311502241
+public class MyClassDrive {
+    public static void main(String[] args) {
+        MyClass info = new MyClass();
+        System.out.println(info.name);
+        System.out.println(info.surname);
+        System.out.println(info.age);
+        System.out.println(info.department);
+        System.out.println(info.beard);
+        System.out.println(info.height);
+        System.out.println(info.mustache);
+        System.out.println(info.hobby);
+        System.out.println(info.university);
+    }
+}
