d65d127eeb223e54be076d88a9b8e1b516725210
Gurhan <161501441+GurhanKaya@users.noreply.github.com>
2024-04-01 15:52:28 +0300

    GurhanK_OOP6.1
    Variable Dosyası ve Kişi bilgilerinin tanımlandığı dosyalar

---
+++ Homework_week6/GurhanK_KisiBilgi/GurhanK_KisiBilgi.java
@@ -0,0 +1,11 @@
+public class GurhanK_KisiBilgi {
+    String isim = "Gurhan";
+    String soyad= "Kaya";
+    int yas = 21;
+    String bolum = "Bilgisyar Muh";
+    String sacRenk = "Siyah";
+    double boy = 186.0;
+    Boolean biyik = true;
+    Boolean sakal = true;
+    String hobi = "Kodlama";
+}
