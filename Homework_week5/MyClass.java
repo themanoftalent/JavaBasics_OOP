3a2aa73dbaed37b658b6da1e48a695459494bc84
Yusuf Mermertaş <154246812+yusufmrmrts18@users.noreply.github.com>
2024-04-01 15:46:34 +0300

    Yusuf Mermertaş 5. hafta ödevi

---
+++ Homework_week5/MyClass.java
@@ -0,0 +1,15 @@
+public class MyClass {
+
+    public String name="Yusuf";
+    public String surName="Mermertaş";
+    public int age=19;
+    public String universtiy="Bandirma 17 Eylül Üniversitesi";
+    public String department="Computer Engineer";
+    public String hairColor="black";
+    public int heigth=170;
+    public boolean mustache =false;
+    public boolean beard=false;
+    public String myHobby="horse riding";
+
+
+}
