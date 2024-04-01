44173422471e560659d63f9383aaec176b4fe263
meltemflz <161947642+meltemflz@users.noreply.github.com>
2024-04-01 16:12:34 +0300

    Meltem Filiz 2311502205

---
+++ src/myclasss/MyClass.java
@@ -0,0 +1,12 @@
+package myclasss;
+
+public class MyClass {
+	public String name = "Meltem";
+    public String surname = "Filiz";
+    public int age = 19;
+    public String university = "Bandirma";
+    public String department = "Computer Engineering";
+    public String hairColor = "Black";
+    public int height = 172;
+  
+}
