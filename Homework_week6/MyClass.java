7067eb045b630ea0f30f964135ad96c1be9e521d
Nisanur Goren <162514380+NisanurGoren@users.noreply.github.com>
2024-04-01 15:56:08 +0300

    Nisanur Gören

---
+++ Homework_week6/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+        public int tarih=2004;
+       public String isim="nisanur";
+        public String soyisim="goren";
+       public int boy=175;
+       public String hobby="voleybol";
+       public String sacRengi="kahve";
+       public int yas=20;
+       public String bolum="mühendislik";
+
+}
