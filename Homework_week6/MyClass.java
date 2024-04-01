1eb1da35174b5681e1d7ee5956eaf5ca6bcb2323
pelitisil <162353050+pelitisil@users.noreply.github.com>
2024-04-01 15:51:23 +0300

    Zeynep Işıl Pelit

---
+++ Homework_week6/MyClass.java
@@ -0,0 +1,11 @@
+public class MyClass {
+        public int tarih=2004;
+       public String isim="isil";
+        public String soyisim="pelit";
+       public int boy=175;
+       public String hobby="voleybol";
+       public String sacRengi="kahve";
+       public int yas=20;
+       public String bolum="mühendislik";
+
+}
---
+++ Homework_week6/MyClassDriver.java
@@ -0,0 +1,13 @@
+public class MyClassDriver {
+    public static void main(String[] args) {
+        MyClass ornek = new MyClass();
+        System.out.println(ornek.boy);
+        System.out.println(ornek.hobby);
+        System.out.println(ornek.soyisim);
+        System.out.println(ornek.sacRengi);
+        System.out.println(ornek.isim);
+        System.out.println(ornek.tarih);
+        System.out.println(ornek.yas);
+        System.out.println(ornek.bolum);
+    }
+}
