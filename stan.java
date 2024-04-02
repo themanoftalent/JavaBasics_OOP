b2d6a29eceaf731951f083b0c57b4111329cb68c
kostkastanis <163309981+kostkastanis@users.noreply.github.com>
2024-04-02 14:40:29 +0300

    Stanislas Kostka Sidibe 2311502266

---
+++ stan.java
@@ -0,0 +1,13 @@
+public class stan {
+        public String name = "Stanislas";
+        public String surname = "Sidibe";
+        public int age = 19;
+        public String university = "Bandirma";
+        public String department = "Computer Engineering";
+        public String hairColor = "black";
+        public int height = 186;
+        public boolean mustache = true;
+        public boolean beard = true;
+        public String hobby= "football";
+    }
+
---
+++ standrive.java
@@ -0,0 +1,16 @@
+public class standrive {
+    public static void main(String[] args) {
+
+                stan info = new stan();
+                System.out.println(info.name);
+                System.out.println(info.surname);
+                System.out.println(info.age);
+                System.out.println(info.department);
+                System.out.println(info.beard);
+                System.out.println(info.height);
+                System.out.println(info.mustache);
+                System.out.println(info.hobby);
+                System.out.println(info.university);
+            }
+        }
+
