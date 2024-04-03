9a82962f076e7d98568ae567a5e84c28e9e293a0
Mehmet Akif Cifci <akif.cifci@tuwien.ac.at>
2024-04-03 17:43:18 +0300

    Updated

--- Homework_week6/Myclass.java
+++ Homework_week6/Myclass.java
@@ -1,3 +1,16 @@
+ main
+// Arda Göktaş 2311502241
+public class MyClass {
+    public String name = "Arda";
+    public String surname = "Goktas";
+    public int age = 20;
+    public String university = "Bandirma";
+    public String department = "Computer Engineering";
+    public String hairColor = "Brown";
+    public int height = 184;
+    public boolean mustache = true;
+    public boolean beard = true;
+    public String hobby= "Basketball";
 public class Myclass {
     public String isim = "Baran";
     public String soyisim ="Kanat";
@@ -8,5 +21,5 @@ public class Myclass {
     public boolean bıyık = true;
     public boolean sakal = true;
     public String hobi = "F1";
-
+main
 }
