478559f352d5e46a96edbd67ea41fc2f3eb85d32
Mehmet Akif Cifci <akif.cifci@tuwien.ac.at>
2024-04-03 08:39:48 +0300

    updated

--- Homework_week6/MyClass.java
+++ Homework_week6/MyClass.java
@@ -1,28 +1,12 @@
-
-package myclass;
-
-public class MyClass {
-
-	    public String name = "Beyza";
-	    public String surname = "Ayan";
-	    public int age = 22;
-	    public String university = "Bandirma";
-	    public String department = "Computer Engineering";
-	    public String hairColor = "Brown";
-	    
-	
+public class Myclass {
+    public String isim = "Baran";
+    public String soyisim ="Kanat";
+    public  int yas = 21;
+    public String uni = "Bandırma 17 Eylül Üni";
+    public String saç ="Siyah";
+    public int boy = 180;
+    public boolean bıyık = true;
+    public boolean sakal = true;
+    public String hobi = "F1";
 
 }
-
-public class MyClass {
-        public int tarih=2004;
-       public String isim="isil";
-        public String soyisim="pelit";
-       public int boy=175;
-       public String hobby="voleybol";
-       public String sacRengi="kahve";
-       public int yas=20;
-       public String bolum="mühendislik";
-
-}
-
--- Homework_week6/MyClassDrive.java
+++ Homework_week6/MyClassDrive.java
@@ -1,17 +1,14 @@
-package myclass;
-
-public class MyClassDrive {
-	
-	    public static void main(String[] args) {
-	        MyClass info = new MyClass();
-	        System.out.println(info.name);
-	        System.out.println(info.surname);
-	        System.out.println(info.age);
-	        System.out.println(info.department);
-	       System.out.println(info.university);
-	    
-	}
-	
-	
-	
+public class Myclassdrive {
+    public static void main(String[] args) {
+        Myclass call = new Myclass();
+        System.out.println(call.isim);
+        System.out.println(call.soyisim);
+        System.out.println(call.yas);
+        System.out.println(call.uni);
+        System.out.println(call.boy);
+        System.out.println(call.saç);
+        System.out.println(call.bıyık);
+        System.out.println(call.sakal);
+        System.out.println(call.hobi);
+    }
 }
