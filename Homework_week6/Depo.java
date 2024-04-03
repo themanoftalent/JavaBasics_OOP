fdff815f904e099b26eb93b339edcce1b3bb5ef7
BKaanSahin <162595080+BKaanSahin@users.noreply.github.com>
2024-04-03 16:47:42 +0300

    Kaan Şahin 2311502223

---
+++ Homework_week6/Depo.java
@@ -0,0 +1,14 @@
+public class Depo {
+
+        String isim = "Kaan";
+        String soyisim = "Şahin";
+        int yaş = 19;
+        double  doğumTarihi = 2005;
+        String saçRengi = "Siyah";
+        int boy = 183;
+        int kilo = 65;
+        String bolum = "Bilgisayar Mühendisliği";
+
+
+
+}
---
+++ Homework_week6/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+       Depo Object = new Depo();
+        System.out.println("İsim: " + Object.isim);
+        System.out.println("Soyisim: " +Object.soyisim);
+        System.out.println("Yaş: " + Object.yaş);
+        System.out.println("Doğum Tarihi: " + Object.doğumTarihi);
+        System.out.println("Saç Rengi: " + Object.saçRengi);
+        System.out.println("Boy: " + Object.boy);
+        System.out.println("Kilo: " + Object.kilo);
+        System.out.println("Bölüm: " + Object.bolum);
+
+    }
+}
