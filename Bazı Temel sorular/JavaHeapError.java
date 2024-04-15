14ed2090f81fba666630edb4220fc20c3c63ad4d
ozlemsenn <164781192+ozlemsenn@users.noreply.github.com>
2024-04-15 15:12:58 +0300

    Update JavaHeapError.java

--- Bazı Temel sorular/JavaHeapError.java
+++ Bazı Temel sorular/JavaHeapError.java
@@ -1,15 +1,14 @@
 public class HeapExample {
     public static void main(String[] args) {
-        // dizi ve dizi heap belleğine yazdırsın
         int[] numbers = new int[5];
 
-        // numbers dizisine elemanları atayalım
-
-        for (int i = 0; i < numbers.length; i++) {
-            numbers[i] = i * 10;
+        for(int i=0; i<numbers.length;i++){
+            numbers[i] = i*10;
         }
-        System.out.println("Numbers dizisi içeriği");
-        System.out.println(numbers);
+        for(int i=0; i<numbers.length; i++){
+            System.out.print(numbers[i]);
+            System.out.print(" ");
+}
 
     }
 
