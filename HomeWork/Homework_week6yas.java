5bc08e5c9805a4fff848d94b765c6e6b7b3537de
eslemey <162445685+eslemey@users.noreply.github.com>
2024-04-15 16:55:51 +0300

    Create Homework_week6yas.java

---
+++ HomeWork/Homework_week6yas.java
@@ -0,0 +1,14 @@
+public class MyClassOneFiles {
+    public String isim;
+    public String soyisim;
+    public int dogum_tarihi;
+    public int bugunku_tarih;
+    public int yas = bugunku_tarih-dogum_tarihi;
+    public String uni;
+    public  String  bolum;
+    public String sacrengi;
+
+
+
+
+}
