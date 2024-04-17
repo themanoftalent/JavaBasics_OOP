ae0746a5dcfaefccae8c8433211fc2c4b40648a9
meltemflz <161947642+meltemflz@users.noreply.github.com>
2024-04-18 02:35:12 +0300

    Meltem Filiz 2311502205 (ödev 7,8,9)

---
+++ HomeWork/Homework_week3/asalsayi.java
@@ -0,0 +1,20 @@
+package asalsayi;
+import java.util.Scanner;
+
+public class asalsayi {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sayı giriniz: ");
+        int number = sc.nextInt();
+        boolean asalmi = true;
+
+        for(int i = 2; i<number/2; i++){
+            if(number % i == 0){
+                asalmi = false;
+                break;
+            }
+        }
+        if(asalmi) System.out.println("Asal");
+        else System.out.println("asal değil.");
+    }
+}  
---
+++ HomeWork/Homework_week3/carpimtablosu.java
@@ -0,0 +1,16 @@
+package carpimtablosu;
+import java.util.Arrays;
+public class carpimtablosu {
+	public static void main(String[] args) {
+	       int[][] dizi = new int[10][10];
+
+	        for(int i = 0; i<10;i++){
+	            for(int j = 0; j<10; j++){
+	                dizi[i][j] = i * j;
+	            }
+	        }
+	        for(int i = 0; i<10;i++){
+	            System.out.println(Arrays.toString(dizi[i]));
+	        }
+	    }
+	}
