776fc3d5f82b1a9b62d69703c6a7c739ca0e0c7e
Segbe Abdoul Aziz Diomande <153992448+lezizouu@users.noreply.github.com>
2024-04-18 15:21:45 +0300

    HelloWord.java

--- Homework_week3/Main.java
+++ Homework_week3/Main.java
@@ -1,19 +1,7 @@
-import java.util.Scanner;
-public class Main {
-
+//Segbe Abdoul Aziz Diomande
+public class Assigment1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String name;
-        System.out.println("enter your name");
-        name = scanner.next();
-        String surname;
-        System.out.println("enter your surname");
-        surname = scanner.next();
-        if (name.equals("aziz") && surname.equals("diomande")) {
-            System.out.println("your name and surname are correct");
-        }
-        else {
-            System.out.println("Your name is incorrect!!!");
-        }
+        System.out.println("Hello World!");
     }
+
 }
