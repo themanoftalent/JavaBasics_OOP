61a14be21052b4af33de88712b8484f4287560f3
sümeyye elgin <115994927+s-elg@users.noreply.github.com>
2024-05-06 16:59:31 +0300

    sümeyye elgin 2311502275
    derste yaptığımız try catch çalışması

---
+++ HomeworkWeek8/SumeyyeElginCalculatorMain.java
@@ -0,0 +1,9 @@
+package hafta10;
+
+public class SumeyyeElginCalculatorMain {
+    public static void main(String[] args) {
+        SumeyyeElginCalculator calculator = new SumeyyeElginCalculator();
+
+        calculator.Operations();
+    }
+}
