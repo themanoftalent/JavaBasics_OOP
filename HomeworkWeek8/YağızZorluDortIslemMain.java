e4a67b37efca89dcbc49f4576bea36ecfe6ac063
Yagizzorlu <113940368+Yagizzorlu@users.noreply.github.com>
2024-05-06 21:24:37 +0300

    Yağız Zorlu 2311502270

---
+++ HomeworkWeek8/YağızZorluDortIslemMain.java
@@ -0,0 +1,8 @@
+package Week8;
+
+public class YağızZorluDortIslemMain {
+    public static void main(String[] args) {
+        YağızZorluDortIslemJava dortIslemJava = new YağızZorluDortIslemJava();
+        dortIslemJava.Operations();
+    }
+}
